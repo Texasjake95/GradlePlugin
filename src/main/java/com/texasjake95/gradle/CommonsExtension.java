03547916890910d883a4ba2b8cc987b5ceaeec75
Texasjake95 <texasjake95@gmail.com>
2014-03-06 14:38:39 -0600

    Version Bump for Commons

--- src/main/java/com/texasjake95/gradle/CommonsExtension.java
+++ src/main/java/com/texasjake95/gradle/CommonsExtension.java
@@ -3,7 +3,7 @@ package com.texasjake95.gradle;
 
 public class CommonsExtension {
 	
-	public String version = "1.0.42";
+	public String version = "1.0.43";
 	public boolean shouldDownload = true;
 	
 	public CommonsExtension()
