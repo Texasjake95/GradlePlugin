f094b212e21d4b7354e5cc0fda56491c60418d54
Texasjake95 <texasjake95@gmail.com>
2014-06-30 12:43:05 -0500

    fix signing

--- scripts/success.sh
+++ scripts/success.sh
@@ -2,24 +2,3 @@
 
 # run gradle
 ./gradlew uploadArchives -Psnapshot=true -PmavenUser=${userName} -PmavenPass=${passWord} -Psigning.keyId=${userID} -Psigning.password=${signPass} -Psigning.secretKeyRingFile=temp.gpg
-
-# change to the maven repo directory
-#cd ./maven-repo
-
-# add correct origin to the repo 
-#git remote rm origin
-#git remote add origin https://Texasjake95:${GH_TOKEN}@github.com/Texasjake95/maven-repo.git
-
-# configure repo
-#git config user.email "texasjake95@gmail.com"
-#git config user.name "Texasjake95"
-
-# configure git push
-#git config push.default current
-
-# preform commit
-#git add .
-#git commit -q -m "Travis-CI Build Push"
-
-# push commit
-#git push -q
