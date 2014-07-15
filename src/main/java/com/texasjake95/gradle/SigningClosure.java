411c9c37feb1ed4db53ca309482255a6ed796533
Texasjake95 <texasjake95@gmail.com>
2014-07-15 10:58:27 -0500

    Make the Closure a Closure

--- src/main/java/com/texasjake95/gradle/SigningClosure.java
+++ src/main/java/com/texasjake95/gradle/SigningClosure.java
@@ -1,20 +1,22 @@
 package com.texasjake95.gradle;
 
-import java.util.concurrent.Callable;
+import groovy.lang.Closure;
 
 import org.gradle.api.Project;
 
-public class SigningClosure implements Callable<Boolean> {
+public class SigningClosure extends Closure<Boolean> {
 
+	private static final long serialVersionUID = 8489621936872239815L;
 	Project project;
 
 	public SigningClosure(Project project)
 	{
+		super(project);
 		this.project = project;
 	}
 
 	@Override
-	public Boolean call() throws Exception
+	public Boolean call()
 	{
 		if (project.getGradle().getTaskGraph().hasTask("uploadArchives"))
 			if (project.hasProperty("signJars"))
